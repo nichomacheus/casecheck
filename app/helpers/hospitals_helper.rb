38c044b341fc1a8a8d666b005f0a337b375a30dd
Nicholas Aldridge <nicholas.g.aldridge@gmail.com>
2019-02-03 17:54:55 -0600

    create hospital record structure and update readme

--- app/helpers/hospitals_helper.rb
+++ app/helpers/hospitals_helper.rb
@@ -1,2 +1,12 @@
 module HospitalsHelper
+	
+	def send_response(response)
+		puts "****************"
+		puts response
+		puts "****************"
+		respond_to do |format|
+			format.json { render :json => response}
+		end
+	end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# grab the list of hospitals
+hospitals = JSON.parse(File.read(Rails.root.join('coding-challenge-assets','hospitals.json')))
+
+hospitals.each do |hospital|
+	# convert to hash
+	h = hospital.to_h 
+	# set hospital id == id (records in the db will automatically have an id column)
+	h[:hospital_id] = h[:id]
+	# delete the id entry
+	h.delete(:id)
+	# create the record
+  	Hospital.create(h)
+end
