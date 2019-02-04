65656ebf7fd06e3418d380ca7ad8383b7a002d86
Nicholas Aldridge <nicholas.g.aldridge@gmail.com>
2019-02-03 18:28:59 -0600

    create controller

--- README.md
+++ README.md
@@ -6,5 +6,25 @@
 
 * You should create a PostgreSQL database called casecheck_development for development testing of this API. Should you wish to alter the database name or other specifics related to the database do so in the config/database.yml file.
 
+* Once you have created the database, run:
 
+	$ rails db:migrate
+
+* This will instantiate the database tables in the appropraite way. You may then run: 
+
+	$ rails db:seed
+
+* This will seed the database with the provided data. Start a local rails server by running:
+
+	$ rails s
+
+* You can now access the api at http://localhost:3000. The endpoints are included below
+
+* ENDPOINTS: 
+
+	GET /hospitals  -- the collection of hospitals
+	GET /hospitals/:id -- see one hospital
+	POST /hospitals -- create a new hospital, expects an object of the form {hospital: {-- put hospital object here --}}
+	PUT /hospitals/:id -- update a hospital, expects an object of the form {hospital: {-- put new key/value pairs here (this must include the id of the hospital)}}
+	DELETE /hospitals/:id -- delete a hospital
 
--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -1,2 +1,84 @@
 class HospitalsController < ApplicationController
+	include HospitalsHelper
+
+	def index
+		# if we are given a city parameter filter by this
+		if params[:city]
+			h = Hospital.where({city: params[:city]})
+		# if not get all the hospitals
+		else 
+			h = Hospital.all
+		end
+		@hospitals = h
+		# respond with json
+		send_response(h)
+	end
+
+	def show
+		# if we got an id, find the hospital by that hospital id
+		if params[:id] then r = Hospital.find_by_hospital_id(params[:id])
+		# otherwise resppond with error 
+		else r = "Please specify an id paramter" end
+		send_response(r)
+	end
+
+	def create
+		# if we got parameters
+		if hospital_params
+			# convert the id key to hospital id key
+			hospital_params[:hospital_id] = hospital_params[:id]
+			hospital_params.delete(:id)
+			# create the hospital record
+			r = Hospital.create(hospital_params)
+		# otherwise respond with error
+		else r = "Please specify hospital info for add" end 
+		send_response(r)
+	end
+
+	def update
+		# if we got hospital parameters and those parameters include an id tag
+		if hospital_params && hospital_params[:id]
+			# find the appropriate hospital
+			h = Hospital.find_by_hospital_id(hospital_params[:id])
+			# perform the update
+			h.update_attributes(hospital_params)
+			# save && respond with the record
+			r = h.save ? "Successfully updated the hospital parameters" : "Update error occured"
+		else 
+			# otherwise throw error
+			r = "Please specify update params"
+		end
+		send_response(r)
+
+	end
+
+	def destroy
+		# if we received the id to destroy
+		if params[:id]
+			# find the record 
+			h = Hospital.find_by_hospital_id(params[:id])
+			# if the record exists ....
+			if h
+				# destroy the record
+				h.destroy
+				# respond with success
+				r = "Successfully deleted the hospital record" 
+			else
+				# error -- hospital not found
+				r = "Hospital not found"
+			end
+		else
+			# error -- no hospital id given
+			r = "Please specify a hospital id associatd with the hospital to be deleted"
+		end
+		send_response(r)
+	end
+
+	private
+
+	# only allow certain params 
+	def hospital_params
+		params.fetch(:hospital, {})
+	end
+
 end
